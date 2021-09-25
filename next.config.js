f9448781b0925b6b3952ca4a57667dfe29f7bb70
fadizakharia <fadi.zakharia@icloud.com>
2021-09-25 16:46:44 +0300

    removed environment variabels from config

--- next.config.js
+++ next.config.js
@@ -18,11 +18,4 @@ module.exports = {
 
     return config;
   },
-
-  env: {
-    MAIL_CHIMP_URL: "YGGarcia.us5.list-manage.com",
-    MAIL_CHIMP_USER: "9b12a7395b94d2a533746ba84",
-    MAIL_CHIMP_ID: "e3fa2be9a0",
-    APOLLO_URL: "http://localhost:5000/graphql",
-  },
 };
