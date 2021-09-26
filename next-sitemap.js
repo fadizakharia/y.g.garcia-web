322bc5424fe7f717af186a9596ff553bfd7ffca5
fadizakharia <fadi.zakharia@icloud.com>
2021-09-26 20:44:55 +0300

    added sitemaps generator to allow seo

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+const siteUrl = "https://www.yggarcia.com";
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
