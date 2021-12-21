00c0c7c5b200d5e8cb416d44977045c60142475a
fadi <fadi.zakharia@icloud.com>
2021-12-21 23:55:32 +0200

    upgraded modules

--- next.config.js
+++ next.config.js
@@ -21,11 +21,4 @@ module.exports = {
   images: {
     domains: ["s3.us-east-2.amazonaws.com"],
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
